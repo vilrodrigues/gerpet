a50a61319611d3faa530f74b85a2c16fe7b3313a
vilrodrigues <vilrodriguesjr@gmail.com>
2022-06-10 19:57:46 -0300

    added db connection and login page

---
+++ conexao.php
@@ -0,0 +1,41 @@
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "login_acesso";
+
+$connect = mysqli_connect($servername, $username, $password, $db_name);
+
+if(isset($_POST['btn-entrar'])){
+    $login = mysqli_escape_string($connect, $_POST['login']);
+    $senha = mysqli_escape_string($connect, $_POST['senha']);
+
+    if(empty($login) or empty($senha)){
+        echo 'Favor preencher os dados';
+    } 
+    else {
+        $sql = "SELECT login FROM usuarios WHERE login = '$login';";
+        $resultado = mysqli_query($connect, $sql);
+
+        if(mysqli_num_rows($resultado) > 0){
+            $senha = md5($senha);
+            $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha';";
+            $resultado = mysqli_query($connect, $sql);
+
+            if(mysqli_num_rows($resultado) == 1){
+                $dados = mysqli_fetch_array($resultado);
+                $_SESSION['logado'] = true;
+                $_SESSION["id_usuario"] = $dados['id'];
+                
+                header('location: conectado.php');
+            }
+        } 
+        else 
+            echo "Usuário Inexistente";
+        }
+}
+    
+if(mysqli_connect_error() == true){
+    echo "Falha na conexão" . mysqli_connect_error();
+}
---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GERPET</title>
+</head>
+<body>
+    <h1>Login de Acesso</h1>
+    <form action="conexao.php" method="POST">
+        Login: <input type="text" name="login" value="" /><br>
+        Senha: <input type="password" name="senha" value="" /><br>
+        <button type="submit" name="btn-entrar">Entrar </button>
+    </form>
+</body>
+</html>
